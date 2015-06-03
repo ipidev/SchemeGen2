0a0ddf267c76dbfb4ffeee6091dbbc39391467da
ipidev <ipimusicdev@gmail.com>
2015-06-03 23:16:01 +0100

    Added value generator base class and set value generator. Needs to be used by the XML parser via some "scheme template" object...

---
+++ SchemeGen2/Randomisation/SetValueGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchemeGen2.Randomisation
+{
+    /// <summary>
+    /// An value generator that only returns the value it was constructed with. 
+    /// </summary>
+    class SetValueGenerator : ValueGenerator
+    {
+        public SetValueGenerator(byte value) :
+            base()
+        {
+            _value = value;
+        }
+
+        public override byte GenerateByte()
+        {
+            return _value;
+        }
+
+        byte _value;
+    }
+}
---
+++ SchemeGen2/Randomisation/ValueGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchemeGen2.Randomisation
+{
+    /// <summary>
+    /// The base class for a value generator. Objects deriving from this type
+    /// can be used to generate values for use in the scheme.
+    /// </summary>
+    abstract class ValueGenerator
+    {
+        /// <summary>
+        /// If set, a set seed used for generating reproducable results will be used.
+        /// </summary>
+        static bool UseDebugSeed = true;
+
+        /// <summary>
+        /// Constructs a new value generator.
+        /// </summary>
+        public ValueGenerator()
+        {
+            if (UseDebugSeed)
+            {
+                _rng = new Random(17);
+            }
+            else
+            {
+                _rng = new Random();
+            }
+        }
+
+        /// <summary>
+        /// When overridden in a deriving type, generates a byte for use in the scheme.
+        /// </summary>
+        /// <returns>A byte value.</returns>
+        public abstract byte GenerateByte(); 
+
+        //All value generators will use the same RNG so that a seed can be used
+        //to give reproducible results.
+        protected Random _rng;
+    }
+}
